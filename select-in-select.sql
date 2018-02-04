068760a5ab0dda56e3ed05f3c0b1abda03cec6fb
joananjin <joananjin@163.com>
2018-02-04 13:00:19 +0800

    Update select-in-select.sql

--- select-in-select.sql
+++ select-in-select.sql
@@ -35,7 +35,7 @@ ORDER BY name
 /*
 Which country has a population that is more than Canada but less than Poland? Show the name and the population.
 */
-SELECT name, population
+SELECT name,population
 FROM world
 WHERE population >
     (SELECT population FROM world WHERE name = 'Canada')
