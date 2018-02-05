80c75f9d1dd8cfc0c61cf7705be8c98a97a686b1
joananjin <joananjin@163.com>
2018-02-05 21:11:26 +0800

    Update using-null.sql

--- using-null.sql
+++ using-null.sql
@@ -60,6 +60,7 @@ FROM teacher
 /*
 Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
 */
+/*count不计算值为null的值列*/
 SELECT dept.name, COUNT(teacher.name)
 FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
 GROUP BY dept.name
